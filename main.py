e8f9c73e4140146e3c1d46451a5f943b474b8f1c
Sonu Kumar(Viral Boy Bihar)-sonukumarviralboy <sonuramashishnpm@gmail.com>
2025-12-21 17:49:09 +0530

    Update URL for llama3.2 model

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def handler(data:LLMRequest):
       if not data.model in models_state:
         if data.model=="llama3.2":
           models_state.append(data.model)
-          url="https://cycle-cells-characteristic-standing.trycloudflare.com"
+          url="https://java-constraints-trades-subcommittee.trycloudflare.com"
         elif data.model=="gemma:3b":
           models_state.append(data.model)
           url=""
