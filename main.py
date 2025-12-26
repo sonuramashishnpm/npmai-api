63f2a2822e5698f2d6dae2d467e98ad85ae82e41
Sonu Kumar <sonuramashishnpm@gmail.com>
2025-12-26 18:35:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class LLMRequest(BaseModel):
     streaming: bool = False
 
 Model_links = {
-    "llama3.2": "https://jones-hereby-petersburg-building.trycloudflare.com/llm"
+    "llama3.2": "https://sensitive-commitments-genres-piece.trycloudflare.com/llm"
 }
 
 @app.post("/llm")
