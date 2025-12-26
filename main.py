adbed46462bfadd27cc14bd6a02f0382a289bafb
Sonu Kumar <sonuramashishnpm@gmail.com>
2025-12-26 20:07:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
-import httpx
+import requests
 
 app = FastAPI()
 
@@ -9,7 +8,6 @@ class LLMRequest(BaseModel):
     prompt: str
     model: str
     temperature: float = 0.7
-    streaming: bool = False
 
 Model_links = {
     "llama3.2": "https://sensitive-commitments-genres-piece.trycloudflare.com/llm"
@@ -26,25 +24,9 @@ async def handler(data: LLMRequest):
     payload = {
         "prompt": data.prompt,
         "temperature": data.temperature,
-        "streaming": data.streaming
     }
 
     url = Model_links[data.model]
 
-    if data.streaming:
-        async def stream():
-            async with httpx.AsyncClient(timeout=None) as client:
-                async with client.stream("POST", url, json=payload) as r:
-                    async for line in r.aiter_lines():
-                        if line:
-                            yield line + "\n"
-
-        return StreamingResponse(
-            stream(),
-            media_type="text/event-stream"
-        )
-
-    async with httpx.AsyncClient() as client:
-        r = await client.post(url, json=payload)
-        r.raise_for_status()
-        return r.json()
+    response=requests.post(url,json=payload)
+    return response["response"]
