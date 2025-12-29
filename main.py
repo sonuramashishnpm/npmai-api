ab364bc7445a83ae3d6b279a3f7582aafd648f10
Sonu Kumar <sonuramashishnpm@gmail.com>
2025-12-29 10:15:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class LLMRequest(BaseModel):
     validated_schema: Optional[Dict[str, Any]] = None 
 
 Model_links = {
-    "llama3.2": "https://modified-ram-periodic-addressed.trycloudflare.com/llm" 
+    "llama3.2": "https://ways-remains-sailing-roller.trycloudflare.com/llm" 
 } 
 
 @app.post("/llm") 
